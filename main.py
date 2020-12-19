5ac4999e73388e486c6eb14988e8a7de4b690e68
zachhuang610 <zachary_huagn@brown.edu>
2020-12-19 17:55:35 -0500

    cleaned up code a little

--- main.py
+++ main.py
@@ -1,22 +1,10 @@
-
 import praw
 import re
 import pandas as pd
 import config
-def get_tickers():
-    reddit = praw.Reddit(
-        client_id= config.api_id,
-        client_secret= config.api_secret,
-        user_agent="WSB Scraping",
-    )
-    to_buy = []
-    to_sell = []
-    prev = open("prev.txt", "w+")
-    prev_tickers = prev.readlines()
-    prev_tickers = [x.strip() for x in prev_tickers]
-    weekly_tickers = {}
-    regex_pattern = r'\b([A-Z]+)\b'
-    phrases = {}
+
+
+def get_stock_list():
     ticker_dict = {}
     filelist = ["list1.csv", "list2.csv", "list3.csv"]
     for file in filelist:
@@ -24,6 +12,27 @@ def get_tickers():
         tl = tl[tl.columns[0]].tolist()
         for ticker in tl:
             ticker_dict[ticker] = 1
+    return ticker_dict
+
+
+def get_prev_tickers():
+    prev = open("prev.txt", "w+")
+    prev_tickers = prev.readlines()
+    prev_tickers = [x.strip() for x in prev_tickers]
+    return prev, prev_tickers
+
+
+def get_tickers():
+    reddit = praw.Reddit(
+        client_id=config.api_id,
+        client_secret=config.api_secret,
+        user_agent="WSB Scraping",
+    )
+    prev, prev_tickers = get_prev_tickers()
+    weekly_tickers = {}
+
+    regex_pattern = r'\b([A-Z]+)\b'
+    ticker_dict = get_stock_list()
     blacklist = ["A", "I", "DD", "WSB", "YOLO", "RH"]
     for submission in reddit.subreddit("wallstreetbets").top("week"):
         strings = [submission.title]
@@ -39,12 +48,16 @@ def get_tickers():
                         else:
                             weekly_tickers[phrase] += 1
     top_tickers = sorted(weekly_tickers, key=weekly_tickers.get, reverse=True)[:5]
+    top_tickers = [ticker + '\n' for ticker in top_tickers]
+
+    to_buy = []
+    to_sell = []
     for new in top_tickers:
         if new not in prev_tickers:
-            to_buy.append(new+'\n')
+            to_buy.append(new)
     for old in prev_tickers:
         if old not in top_tickers:
-            to_sell.append(old+'\n')
+            to_sell.append(old)
 
     prev.writelines(top_tickers)
     prev.close()
